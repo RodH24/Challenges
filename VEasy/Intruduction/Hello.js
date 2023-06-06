0b1d7eacedeb253c9dbc04b9122ffd857d0923e1
RodH24 <hm.rodriguezgomez24@gmail.com>
2023-06-06 12:32:58 -0600

    first commit

---
+++ VEasy/Intruduction/Hello.js
@@ -0,0 +1,5 @@
+function hello() {
+	return "hello edabit.com";
+}
+
+// const hello = () => "hello edabit.com";
---
+++ VEasy/RFirst/RFirst.js
@@ -0,0 +1,16 @@
+function getFirstValue(arr) {
+	const first = arr[0]
+	return first
+}
+
+// function getFirstValue(arr) {
+// 	return arr[0];
+// }
+
+// const getFirstValue = arr => arr[0];
+
+// function getFirstValue(arr) {
+// 	return arr.shift();
+// }
+
+// var getFirstValue = arr => Array.isArray(arr) ? arr[0] : "error";
