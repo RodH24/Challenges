73126f5791727158001b8e7c8d282ba73fb2f80f
RodH24 <hm.rodriguezgomez24@gmail.com>
2023-06-07 13:18:25 -0600

    Something Challenges

---
+++ VEasy/Something/Something.js
@@ -0,0 +1,10 @@
+function giveMeSomething(a) {
+	const b = ("something ");
+	return b.concat(a);
+    // return `something ${a}`;
+    // return "something " + a;
+}
+
+// const giveMeSomething = a => `something ${a}`;
+// const giveMeSomething = str => ['something', str].join(' ');
+
