d50b5ae5e37384821e72a5ceecbd1896c57911a8
RodH24 <hm.rodriguezgomez24@gmail.com>
2023-06-07 13:06:57 -0600

    PowerC Complete  Challenges

--- VEasy/PowerC/PowerC.js
+++ VEasy/PowerC/PowerC.js
@@ -2,4 +2,4 @@ function circuitPower(voltage, current) {
 	return voltage * current;
 }
 
-const circuitPower=(voltage, current)=>voltage*current
+// const circuitPower=(voltage, current)=>voltage*current
