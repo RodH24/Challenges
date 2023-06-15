8f4b233b9f26d145e3bd0dfd375bc10b9b21ab35
RodH24 <hm.rodriguezgomez24@gmail.com>
2023-06-15 13:50:40 -0600

    AgeDays Challenges

---
+++ VEasy/AgeDays/AgeDays.js
@@ -0,0 +1,9 @@
+function calcAge(age) {
+	return age*365;
+    // let ageInDays = (age*365)>=0 && typeof(age*365) == 'number' ? (age*365) : []
+	// return ageInDays
+
+    // return age >= 0 ? age * 365 : undefined
+}
+
+//const calcAge = age => age * 365;
