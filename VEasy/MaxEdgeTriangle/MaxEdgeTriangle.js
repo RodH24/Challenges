23cb70c8ade076730757f20c94cd703d04d24e62
RodH24 <hm.rodriguezgomez24@gmail.com>
2023-07-06 13:21:26 -0600

    MaxEdgeTriangle Challenges

---
+++ VEasy/MaxEdgeTriangle/MaxEdgeTriangle.js
@@ -0,0 +1,10 @@
+function nextEdge(side1, side2) {
+	return (side1 + side2)-1;
+    // if(side1 > 0 && side2 > 0) {
+	// 	return (side1 + side2) - 1;
+	// }else {
+	// 	return;
+	// }
+}
+
+// const nextEdge = (side1, side2) => side1 + side2 -1;
